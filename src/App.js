32b1dd7835caed0d3094febc579b331e93c17149
dhakalsamundra <seapush02@gmail.com>
2020-08-27 20:55:04 +0300

    --no-verify

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import Navbar from './components/layout/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Navbar from './components/layout/NavBar';
+import Alert from './components/layout/Alert';
+import Home from './components/page/Home'
+import GithubState from './context/github/action';
+import AlertState from './context/alert/action';
 
 import './App.css';
 
 const App = () => {
   return (
+    <GithubState>
+      <AlertState>
+        <Router>
           <div className='App'>
             <Navbar />
-            
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+              </Switch>
+            </div>
           </div>
-
+        </Router>
+      </AlertState>
+    </GithubState>
   );
 };
 
